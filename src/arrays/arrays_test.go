75f60d0e6c2ec6372ef4fdc1cd12b2ef21a904e6
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 18:57:55 -0300

    test: add new case to run arrays of any size

--- src/arrays/arrays_test.go
+++ src/arrays/arrays_test.go
@@ -4,17 +4,24 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	result := func(t *testing.T, resultado, esperado int) {
+	result := func(t *testing.T, resultado int, esperado int, dados []int) {
 		t.Helper()
 		if resultado != esperado {
-			t.Errorf("resultado '%d', esperado '%d'", resultado, esperado)
+			t.Errorf("resultado '%d', esperado '%d', dados '%v'", resultado, esperado, dados)
 		}
 	}
 
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 		resultado := Soma(numbers)
 		esperado := 15
-		result(t, resultado, esperado)
+		result(t, resultado, esperado, numbers)
+	})
+
+	t.Run("Collection of any size", func(t *testing.T) {
+		array := []int{1, 2, 3}
+		resultado := Soma(array)
+		esperado := 6
+		result(t, resultado, esperado, array)
 	})
 }
