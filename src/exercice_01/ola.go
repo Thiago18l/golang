f8378e9ba83d1ddcd907ac3704e4bc61a15b5414
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 09:32:23 -0300

    feat: add case for spanish language

--- src/exercice_01/ola.go
+++ src/exercice_01/ola.go
@@ -2,16 +2,22 @@ package main
 
 import "fmt"
 
+const espanhol = "espanhol"
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
 
 // Ola retorna um string
-func Ola(nome string) string {
+func Ola(nome string, lang string) string {
 	if nome == "" {
 		nome = "Mundo"
 	}
+	if lang == espanhol {
+		return prefixoOlaEspanhol + nome
+	}
+
 	return prefixoOlaPortugues + nome
 }
 
 func main() {
-	fmt.Println(Ola(""))
+	fmt.Println(Ola("", ""))
 }
