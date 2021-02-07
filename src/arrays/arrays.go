e1a1844d929ee3828484d69d9a25248749b1620e
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 18:21:41 -0300

    refactor: add range to go through the array

--- src/arrays/arrays.go
+++ src/arrays/arrays.go
@@ -2,8 +2,8 @@ package arrays
 
 // Soma will return the sum of array numbers
 func Soma(numbers [5]int) (soma int) {
-	for i := 0; i < 5; i++ {
-		soma += numbers[i]
+	for _, number := range numbers {
+		soma += number
 	}
 	return soma
 }
