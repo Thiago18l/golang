c1826e1f1a0222c1d8478827722dd84efdc7bc95
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 09:32:05 -0300

    test: add test for spanish language

--- src/exercice_01/ola_test.go
+++ src/exercice_01/ola_test.go
@@ -11,16 +11,24 @@ func TestOla(t *testing.T) {
 	}
 
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Cris")
+		resultado := Ola("Cris", "")
 		esperado := "Olá, Cris"
 
 		verificarMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("'Mundo' como padrão para 'string' vazia", func(t *testing.T) {
-		resultado := Ola("")
+		resultado := Ola("", "")
 		esperado := "Olá, Mundo"
 
 		verificarMensagemCorreta(t, resultado, esperado)
 	})
+
+	t.Run("Em espanhol", func(t *testing.T) {
+		resultado := Ola("Eloide", "espanhol")
+		esperado := "Hola, Eloide"
+
+		verificarMensagemCorreta(t, resultado, esperado)
+	})
+
 }
