2607bc24058b356ef238793eacdd8c38d6aedc86
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 18:48:12 -0300

    refactor: add func result and change the test structure

--- src/arrays/arrays_test.go
+++ src/arrays/arrays_test.go
@@ -3,10 +3,18 @@ package arrays
 import "testing"
 
 func TestSoma(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	resultado := Soma(numbers)
-	esperado := 15
-	if esperado != resultado {
-		t.Errorf("resultado '%d', esperado '%d'", resultado, esperado)
+
+	result := func(t *testing.T, resultado, esperado int) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%d', esperado '%d'", resultado, esperado)
+		}
 	}
+
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+		resultado := Soma(numbers)
+		esperado := 15
+		result(t, resultado, esperado)
+	})
 }
