0184cee3e51cce06d9e7249a88891e7fc9b35a8d
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 18:58:30 -0300

    refactor: change the argument of func to accept slices

--- src/arrays/arrays.go
+++ src/arrays/arrays.go
@@ -1,7 +1,7 @@
 package arrays
 
 // Soma will return the sum of array numbers
-func Soma(numbers [5]int) (soma int) {
+func Soma(numbers []int) (soma int) {
 	for _, number := range numbers {
 		soma += number
 	}
