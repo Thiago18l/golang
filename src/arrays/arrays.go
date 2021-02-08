fdfc824ee0521fbadc4515ad972cdd8b2fb29e16
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 21:50:34 -0300

    refactor:add func 'SomaTodoResto' for sum other slices

--- src/arrays/arrays.go
+++ src/arrays/arrays.go
@@ -7,3 +7,24 @@ func Soma(numbers []int) (soma int) {
 	}
 	return soma
 }
+
+// SomaTudo will return a slice.
+func SomaTudo(numbers ...[]int) (somas []int) {
+	for _, number := range numbers {
+		somas = append(somas, Soma(number))
+	}
+	return somas
+}
+
+// SomaTodoResto will return a slice
+func SomaTodoResto(numbers ...[]int) (soma []int) {
+	for _, number := range numbers {
+		if len(number) == 0 {
+			soma = append(soma, 0)
+		} else {
+			final := number[1:]
+			soma = append(soma, Soma(final))
+		}
+	}
+	return soma
+}
