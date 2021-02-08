40fa6b2743d6a5fac676a0e39dca634f07b14fce
thiago <thiago.lopes.dev@gmail.com>
2021-02-07 21:49:50 -0300

    test: add test for empty slices

--- src/arrays/arrays_test.go
+++ src/arrays/arrays_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
 
@@ -23,5 +26,32 @@ func TestSoma(t *testing.T) {
 		resultado := Soma(array)
 		esperado := 6
 		result(t, resultado, esperado, array)
+	})
+}
+
+func TestSomaTudo(t *testing.T) {
+	verifySum := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado '%v', esperado '%v'", resultado, esperado)
+		}
+	}
+
+	t.Run("Sum all itens inside the slice", func(t *testing.T) {
+		resultado := SomaTudo([]int{1, 2, 3}, []int{1, 1})
+		esperado := []int{6, 2}
+		verifySum(t, resultado, esperado)
+	})
+
+	t.Run("Sum empty slices", func(t *testing.T) {
+		resultado := SomaTodoResto([]int{}, []int{3, 4, 5})
+		esperado := []int{0, 9}
+		verifySum(t, resultado, esperado)
+	})
+
+	t.Run("Sum everything else", func(t *testing.T) {
+		resultado := SomaTodoResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+		verifySum(t, resultado, esperado)
 	})
 }
