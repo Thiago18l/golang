cf2a1927757dd9d231ca242a1ee004786e6cc562
thiago <thiago.lopes.dev@gmail.com>
2021-02-09 13:02:13 +0000

    chore: add 'Area' and the type 'Retangulo'

--- src/structs/struct.go
+++ src/structs/struct.go
@@ -1,6 +1,17 @@
 package structs
 
+// Retangulo sctruct
+type Retangulo struct {
+	Largura float64
+	Altura  float64
+}
+
 // Perimetro will return a float number
 func Perimetro(x, y float64) float64 {
 	return 2 * (x + y)
+}
+
+// Area will return a float number
+func Area(base, altura float64) float64 {
+	return base * altura
 }
