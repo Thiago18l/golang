f5f56cfe34e814b62160be048beedabd846759a0
thiago <thiago.lopes.dev@gmail.com>
2021-02-09 13:01:58 +0000

    test: add test for func 'Area'

--- src/structs/struct_test.go
+++ src/structs/struct_test.go
@@ -10,3 +10,12 @@ func TestPerimetro(t *testing.T) {
 		t.Errorf("result '%.2f', waited '%.2f'", result, waited)
 	}
 }
+
+func TestArea(t *testing.T) {
+	result := Area(12.0, 6.0)
+	waited := 72.0
+
+	if result != waited {
+		t.Errorf("result '%.2f', waited '%.2f'", result, waited)
+	}
+}
