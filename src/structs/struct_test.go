7b9410d6a8d98c435594ccec19025b2fbc4c24d1
thiago <thiago.lopes.dev@gmail.com>
2021-02-09 13:36:59 +0000

    test: add verifyResult to apply DRY

--- src/structs/struct_test.go
+++ src/structs/struct_test.go
@@ -13,11 +13,26 @@ func TestPerimetro(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	retangle := Retangulo{12.0, 6.0}
-	result := Area(retangle)
-	waited := 72.0
-
-	if result != waited {
-		t.Errorf("result '%.2f', waited '%.2f'", result, waited)
+	verifyResult := func(t *testing.T, result, waited float64) {
+		t.Helper()
+		if result != waited {
+			t.Errorf("result '%.2f', waited '%.2f'", result, waited)
+		}
 	}
+
+	t.Run("Rentagles", func(t *testing.T) {
+		retangle := Retangulo{12.0, 6.0}
+		result := retangle.Area()
+		waited := 72.0
+
+		verifyResult(t, result, waited)
+	})
+
+	t.Run("Circles", func(t *testing.T) {
+		circle := Circle{10}
+		result := circle.Area()
+		waited := 314.1592653589793
+
+		verifyResult(t, result, waited)
+	})
 }
