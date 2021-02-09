b11c9f16f48362fcd144d73e988a3e8a99fe2933
thiago <thiago.lopes.dev@gmail.com>
2021-02-09 13:37:42 +0000

    refactor: change the func to a method

--- src/structs/struct.go
+++ src/structs/struct.go
@@ -1,17 +1,29 @@
 package structs
 
-// Retangulo sctruct
+import "math"
+
+// Retangulo struct
 type Retangulo struct {
 	Largura float64
 	Altura  float64
 }
 
-// Perimetro will return a float number
+// Circle struct
+type Circle struct {
+	Raio float64
+}
+
+// Perimetro will return a float64
 func Perimetro(r Retangulo) float64 {
-	return 2 * (r.Altura + r.Largura)
+	return 2 * (r.Largura + r.Altura)
+}
+
+// Area method for Rentagulo
+func (r Retangulo) Area() float64 {
+	return r.Largura * r.Altura
 }
 
 // Area will return a float number
-func Area(r Retangulo) float64 {
-	return r.Largura * r.Altura
+func (c Circle) Area() float64 {
+	return math.Pi * (math.Pow(c.Raio, 2))
 }
