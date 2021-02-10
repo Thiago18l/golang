363f297cc9b125b50602d81fb16c36dc50484d15
thiago <thiago.lopes.dev@gmail.com>
2021-02-10 16:38:06 +0000

    test: add cases to insufficient funds

--- src/pointers/carteira_test.go
+++ src/pointers/carteira_test.go
@@ -3,13 +3,38 @@ package pointers
 import "testing"
 
 func TestCarteira(t *testing.T) {
-	carteira := Carteira{}
-	carteira.Depositar(10)
-
-	result := carteira.Saldo()
-	waited := 10
-
-	if result != waited {
-		t.Errorf("result '%d', waited '%d'", result, waited)
+	verifyOutput := func(t *testing.T, carteira Carteira, waited Bitcoin) {
+		t.Helper()
+		result := carteira.Saldo()
+		if result != waited {
+			t.Errorf("result '%s', waited '%s'", result, waited)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		carteira := Carteira{}
+		carteira.Depositar(Bitcoin(10))
+		waited := Bitcoin(10)
+
+		verifyOutput(t, carteira, waited)
+	})
+
+	t.Run("Cashout", func(t *testing.T) {
+		carteira := Carteira{money: Bitcoin(20)}
+		carteira.Retirar(Bitcoin(10))
+		waited := Bitcoin(10)
+
+		verifyOutput(t, carteira, waited)
+	})
+
+	t.Run("withdraw with insufficient balance", func(t *testing.T) {
+		saldoInicial := Bitcoin(20)
+		carteira := Carteira{saldoInicial}
+		erro := carteira.Retirar(Bitcoin(100))
+
+		verifyOutput(t, carteira, saldoInicial)
+		if erro == nil {
+			t.Errorf("Esperava um erro")
+		}
+	})
 }
