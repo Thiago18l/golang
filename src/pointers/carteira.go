1acd52b30a4903eb6ce679476c63bb0d29e38f3e
thiago <thiago.lopes.dev@gmail.com>
2021-02-10 16:38:33 +0000

    feat: add 'Retirar' method to 'Carteira' struct

--- src/pointers/carteira.go
+++ src/pointers/carteira.go
@@ -1,16 +1,42 @@
 package pointers
 
+import (
+	"errors"
+	"fmt"
+)
+
+// Stringer interface
+type Stringer interface {
+	String() string
+}
+
+// Bitcoin type
+type Bitcoin int
+
 // Carteira struct
 type Carteira struct {
-	money int
+	money Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 // Saldo method
-func (c *Carteira) Saldo() int {
+func (c *Carteira) Saldo() Bitcoin {
 	return c.money
 }
 
 // Depositar method
-func (c *Carteira) Depositar(money int) {
+func (c *Carteira) Depositar(money Bitcoin) {
 	c.money += money
+}
+
+// Retirar method
+func (c *Carteira) Retirar(money Bitcoin) error {
+	if money > c.Saldo() {
+		return errors.New("Saldo insuficiente")
+	}
+	c.money -= money
+	return nil
 }
