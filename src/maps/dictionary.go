bd5ea625574f17194b3a3019901d12017e03915a
thiago <thiago.lopes.dev@gmail.com>
2021-02-11 21:25:33 +0000

    refactor: add type Dictionary and refactor the func 'Busca'

--- src/maps/dictionary.go
+++ src/maps/dictionary.go
@@ -1,11 +1,14 @@
 package maps
 
+// Dictionary type
+type Dictionary map[string]string
+
 // Busca will return a string
-func Busca(dictionary map[string]string, word string) string {
-	return dictionary[word]
+func (d Dictionary) Busca(word string) string {
+	return d[word]
 }
 
 func main() {
-	mapWord := map[string]string{"teste": "isso é um teste"}
-	Busca(mapWord, "teste")
+	dict := Dictionary{"teste": "isso aqui é um teste"}
+	dict.Busca("teste")
 }
