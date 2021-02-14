5d2b2ae3842df802ddbf5a9f13d3360dd158544f
thiago <thiago.lopes.dev@gmail.com>
2021-02-14 21:01:10 +0000

    refactor: alter the type of return of 'Busca'

--- src/maps/dictionary.go
+++ src/maps/dictionary.go
@@ -1,11 +1,20 @@
 package maps
 
+import "errors"
+
+// ErrNotFound details
+var ErrNotFound = errors.New("não foi possível encontrar a palavra que você procura")
+
 // Dictionary type
 type Dictionary map[string]string
 
 // Busca will return a string
-func (d Dictionary) Busca(word string) string {
-	return d[word]
+func (d Dictionary) Busca(word string) (string, error) {
+	definicao, existe := d[word]
+	if !existe {
+		return "", ErrNotFound
+	}
+	return definicao, nil
 }
 
 func main() {
