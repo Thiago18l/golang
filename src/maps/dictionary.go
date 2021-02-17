17b7103ed80d1551ec5e31a882e1951239a35bff
thiago <thiago.lopes.dev@gmail.com>
2021-02-17 13:22:53 +0000

    refactor: add cases of different errors

--- src/maps/dictionary.go
+++ src/maps/dictionary.go
@@ -3,7 +3,10 @@ package maps
 import "errors"
 
 // ErrNotFound details
-var ErrNotFound = errors.New("não foi possível encontrar a palavra que você procura")
+var (
+	ErrNotFound     = errors.New("não foi possível encontrar a palavra que você procura")
+	ErrExistingWord = errors.New("não é possível adicionar a palavra pois ela já existe")
+)
 
 // Dictionary type
 type Dictionary map[string]string
@@ -18,8 +21,17 @@ func (d Dictionary) Busca(word string) (string, error) {
 }
 
 // Adiciona will add a new word to the map
-func (d Dictionary) Adiciona(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Adiciona(word, definition string) error {
+	_, err := d.Busca(word)
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrExistingWord
+	default:
+		return err
+	}
+	return nil
 }
 
 func main() {
