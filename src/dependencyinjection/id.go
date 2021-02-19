d66e371541d9fac47ac63df4e513ce55132c41cc
thiago <thiago.lopes.dev@gmail.com>
2021-02-19 07:35:16 +0000

    feat: add 'Cumprimenta' to output for web

--- src/dependencyinjection/id.go
+++ src/dependencyinjection/id.go
@@ -3,6 +3,7 @@ package dependencyinjection
 import (
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 )
 
@@ -11,6 +12,15 @@ func Cumprimenta(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Olá, %s", name)
 }
 
+// HandleMyMeeting output for web
+func HandleMyMeeting(w http.ResponseWriter, r *http.Request) {
+	Cumprimenta(w, "Mundo")
+}
+
 func main() {
+	err := http.ListenAndServe(":5000", http.HandlerFunc(HandleMyMeeting))
 	Cumprimenta(os.Stdout, "Thiago")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
