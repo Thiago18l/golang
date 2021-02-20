b275cf56403c5ae2dda6140b2f17f30da5ab09f9
thiago <thiago.lopes.dev@gmail.com>
2021-02-20 22:36:17 +0000

    test: add case for paused times and introduce Spy to Sleep

--- src/mocks/contagem_test.go
+++ src/mocks/contagem_test.go
@@ -2,23 +2,81 @@ package mocks
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestContagem(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	sleeperSpy := &SleeperSpy{}
-	Contagem(buffer, sleeperSpy)
+	t.Run("print till 3 Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Contagem(buffer, &SpyContagemOperacoes{})
 
-	result := buffer.String()
-	waited := `3
-	2
-	1
-	Go!`
-	if result != waited {
-		t.Errorf("result '%s', waited '%s'", result, waited)
-	}
-	if sleeperSpy.Chamadas != 4 {
-		t.Errorf("Não houve chamadas suficientes do sleeper, waited 4, result '%d'", sleeperSpy.Chamadas)
-	}
+		resultado := buffer.String()
+		esperado := `3
+2
+1
+Go!`
+
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	})
+
+	t.Run("pause before each print", func(t *testing.T) {
+		spyPrintSleep := &SpyContagemOperacoes{}
+		Contagem(spyPrintSleep, spyPrintSleep)
+
+		waited := []string{
+			pause,
+			write,
+			pause,
+			write,
+			pause,
+			write,
+			pause,
+			write,
+		}
+		if !reflect.DeepEqual(waited, spyPrintSleep.Chamadas) {
+			t.Errorf("waited '%v' calls, result '%v'", waited, spyPrintSleep.Chamadas)
+		}
+	})
+
+	t.Run("Sleeper configurable", func(t *testing.T) {
+		pausedTime := 5 * time.Second
+
+		SpyTime := &TimeSpy{}
+
+		sleeper := SleeperConfigurable{pausedTime, SpyTime.Sleep}
+		sleeper.Sleep()
+		if SpyTime.durationPause != pausedTime {
+			t.Errorf("should paused at %v, but paused at %v", pausedTime, SpyTime.durationPause)
+		}
+	})
+}
+
+type SpyContagemOperacoes struct {
+	Chamadas []string
+}
+
+// Pausa will pause the Sleep()
+func (s *SpyContagemOperacoes) Sleep() {
+	s.Chamadas = append(s.Chamadas, pause)
+}
+
+// Write will put inside the slice the number
+func (s *SpyContagemOperacoes) Write(p []byte) (n int, err error) {
+	s.Chamadas = append(s.Chamadas, write)
+	return
+}
+
+const write = "escrita"
+const pause = "pausa"
+
+type TimeSpy struct {
+	durationPause time.Duration
+}
+
+func (t *TimeSpy) Sleep(duration time.Duration) {
+	t.durationPause = duration
 }
