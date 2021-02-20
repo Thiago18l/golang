4d1e68a200b6cdf36605782c431e187f2e802fca
thiago <thiago.lopes.dev@gmail.com>
2021-02-20 20:42:21 +0000

    feat: add some interface to implement the time.Sleep()

--- src/mocks/main.go
+++ src/mocks/main.go
@@ -4,13 +4,46 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
+const lastWord = "Go!"
+const index = 3
+
+// Sleeper interface
+type Sleeper interface {
+	Sleep()
+}
+
+// SleeperSpy see the number of 'Chamadas'
+type SleeperSpy struct {
+	Chamadas int
+}
+
+// SleeperDefault do nothing at this time
+type SleeperDefault struct{}
+
+// Sleep will add a count to 'Chamadas'
+func (s *SleeperSpy) Sleep() {
+	s.Chamadas++
+}
+
+// Sleep will count the time in seconds
+func (d *SleeperDefault) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 // Contagem will print a output in screen
-func Contagem(exit io.Writer) {
-	fmt.Fprintf(exit, "3")
+func Contagem(exit io.Writer, sleeper Sleeper) {
+	for i := index; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(exit, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(exit, lastWord)
 }
 
 func main() {
-	Contagem(os.Stdout)
+	sleeper := &SleeperDefault{}
+	Contagem(os.Stdout, sleeper)
 }
