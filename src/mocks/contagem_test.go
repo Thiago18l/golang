aa7ca1b73c31ad75ff6385ef46baff8563fd3079
thiago <thiago.lopes.dev@gmail.com>
2021-02-20 20:41:58 +0000

    test: refactor the test case and introducing the Spy

--- src/mocks/contagem_test.go
+++ src/mocks/contagem_test.go
@@ -7,12 +7,18 @@ import (
 
 func TestContagem(t *testing.T) {
 	buffer := &bytes.Buffer{}
-
-	Contagem(buffer)
+	sleeperSpy := &SleeperSpy{}
+	Contagem(buffer, sleeperSpy)
 
 	result := buffer.String()
-	waited := "3"
+	waited := `3
+	2
+	1
+	Go!`
 	if result != waited {
 		t.Errorf("result '%s', waited '%s'", result, waited)
+	}
+	if sleeperSpy.Chamadas != 4 {
+		t.Errorf("Não houve chamadas suficientes do sleeper, waited 4, result '%d'", sleeperSpy.Chamadas)
 	}
 }
