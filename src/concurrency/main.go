a3d34c8791ab98463a919258475262099b3734d4
thiago <thiago.lopes.dev@gmail.com>
2021-02-22 20:56:29 +0000

    refactor: add goroutines to 'VerificaWebSite' func

--- src/concurrency/main.go
+++ src/concurrency/main.go
@@ -3,12 +3,24 @@ package concurrency
 // VerifyWebSite verify an url, and return a boolean
 type VerifyWebSite func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 // VerificaWebSite will receive a type VerifyWebSite and a slice
 func VerificaWebSite(vw VerifyWebSite, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	pipeResult := make(chan result)
 
 	for _, url := range urls {
-		results[url] = vw(url)
+		go func(u string) {
+			pipeResult <- result{u, vw(u)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-pipeResult
+		results[result.string] = result.bool
 	}
 	return results
 }
