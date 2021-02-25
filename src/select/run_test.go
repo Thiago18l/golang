48d6769b4eb5efff256fb110b9a3c8b8692b1a7b
thiago <thiago.lopes.dev@gmail.com>
2021-02-25 15:07:24 -0300

    test: change to httptest to create a server

--- src/select/run_test.go
+++ src/select/run_test.go
@@ -1,10 +1,24 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRunner(t *testing.T) {
-	URLSlow := "https://www.github.com"
-	URLFast := "https://facebook.com"
+	serverSlow := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	serverFast := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	URLSlow := serverSlow.URL
+	URLFast := serverFast.URL
 
 	waited := URLFast
 	result := Runner(URLSlow, URLFast)
