7b56657976539b199eea0c2c31a9fa65f9ded344
thiago <thiago.lopes.dev@gmail.com>
2021-02-27 19:18:02 -0300

    test: add a test case for url with 10 seconds time

--- src/select/run_test.go
+++ src/select/run_test.go
@@ -18,11 +18,24 @@ func TestRunner(t *testing.T) {
 	URLFast := serverFast.URL
 
 	waited := URLFast
-	result := Runner(URLSlow, URLFast)
-
+	result, err := Runner(URLSlow, URLFast, 0*time.Millisecond)
+	if err != nil {
+		t.Fatalf("waited an error, but collect: '%v'", err)
+	}
 	if result != waited {
 		t.Errorf("result '%s', waited '%s'", result, waited)
 	}
+
+	t.Run("return an error if the server don't answer in ten seconds", func(t *testing.T) {
+		server := createServerWithDelay(11 * time.Second)
+
+		defer server.Close()
+
+		_, err := Config(server.URL, server.URL, 20*time.Second)
+		if err == nil {
+			t.Error("was waiting a error")
+		}
+	})
 }
 
 func createServerWithDelay(delay time.Duration) *httptest.Server {
