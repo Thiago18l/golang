114411c01c9041591e0ad9693a31d3e250a987d3
thiago <thiago.lopes.dev@gmail.com>
2021-03-01 19:43:03 -0300

    refactor: add safety to see if the value is a string

--- src/reflection/reflection.go
+++ src/reflection/reflection.go
@@ -4,6 +4,11 @@ import "reflect"
 
 func percorre(x interface{}, fn func(entrada string)) {
 	value := reflect.ValueOf(x)
-	camp := value.Field(0)
-	fn(camp.String())
+
+	for i := 0; i < value.NumField(); i++ {
+		camp := value.Field(i)
+		if camp.Kind() == reflect.String {
+			fn(camp.String())
+		}
+	}
 }
