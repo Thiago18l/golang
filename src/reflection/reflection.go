a08d2d6eb55b53b83c818a27a588ec5351d8c92f
thiago <thiago.lopes.dev@gmail.com>
2021-03-04 11:55:13 -0300

    feat: add 'GetValue' func and change the switch to use recursion in some points

--- src/reflection/reflection.go
+++ src/reflection/reflection.go
@@ -3,15 +3,31 @@ package main
 import "reflect"
 
 func percorre(x interface{}, fn func(entrada string)) {
-	value := reflect.ValueOf(x)
+	value := getValue(x)
 
-	for i := 0; i < value.NumField(); i++ {
-		camp := value.Field(i)
-		switch camp.Kind() {
-		case reflect.String:
-			fn(camp.String())
-		case reflect.Struct:
-			percorre(camp.Interface(), fn)
-		}
+	countOfValues := 0
+	var getCamp func(int) reflect.Value
+
+	switch value.Kind() {
+	case reflect.Struct:
+		countOfValues = value.NumField()
+		getCamp = value.Field
+	case reflect.Slice:
+		countOfValues = value.Len()
+		getCamp = value.Index
+	case reflect.String:
+		fn(value.String())
 	}
+
+	for i := 0; i < countOfValues; i++ {
+		percorre(getCamp(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	value := reflect.ValueOf(x)
+	if value.Kind() == reflect.Ptr {
+		value = value.Elem()
+	}
+	return value
 }
