910421767751f451f3b994d18fa316551d5226df
Thiago <thiago.lopes@trizy.com.br>
2021-03-25 00:11:00 -0300

    test: add func verifycount

--- src/sync/sync_test.go
+++ src/sync/sync_test.go
@@ -3,14 +3,19 @@ package main
 import "testing"
 
 func TestCount(t *testing.T) {
+
+	verifyCount := func(t *testing.T, result Contador, waited int) {
+		t.Helper()
+		if result.Value() != waited {
+			t.Errorf("result %d, waited %d", result.Value(), waited)
+		}
+	}
 	t.Run("Increment the count 3 times result in third value", func(t *testing.T) {
 		contador := Contador{}
 		contador.Increment()
 		contador.Increment()
 		contador.Increment()
 
-		if contador.Value() != 3 {
-			t.Errorf("result %d, waited %d", contador.Value(), 3)
-		}
+		verifyCount(t, contador, 3)
 	})
 }
