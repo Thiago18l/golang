167fde2da502fcd4f046d8f16c8f9c1c000cf3ed
Thiago <thiago.lopes@trizy.com.br>
2021-03-25 00:23:29 -0300

    feat: add NewCount to return a Contador pointer

--- src/sync/sync.go
+++ src/sync/sync.go
@@ -1,10 +1,19 @@
 package main
 
+import "sync"
+
 type Contador struct {
+	mu    sync.Mutex
 	value int
 }
 
+func NewCount() *Contador {
+	return &Contador{}
+}
+
 func (c *Contador) Increment() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
