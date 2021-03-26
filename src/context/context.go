a47178977326b4eaa9f33f8d5d985ab442b5bdd0
Thiago Lopes <thiago.lopes.dev@gmail.com>
2021-03-26 00:25:24 -0300

    chore: add context Server

--- src/context/context.go
+++ src/context/context.go
@@ -1,29 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+	handle := func(rw http.ResponseWriter, r *http.Request) {
+		data, _ := store.Fetch(r.Context())
 
-		data := make(chan string, 1)
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(rw, d)
-		case <-ctx.Done():
-			store.Cancel()
-		}
+		fmt.Fprint(rw, data)
 	}
+	return handle
 }
