e7da74e71d0d650d5aebcf712c0d5438a9f61da1
Thiago <thiago.lopes@trizy.com.br>
2021-03-25 23:32:23 -0300

    test: add testing cases from store

---
+++ src/context/context_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestHandler(t *testing.T) {
+
+	data := "Hello, World"
+
+	t.Run("return data from store", func(t *testing.T) {
+
+		store := &SpyStore{response: data, t: t}
+
+		server := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`result %s, waited "%s`, response.Body.String(), data)
+		}
+		store.assertWasNotCancelled()
+	})
+
+	t.Run("warning the store to cancel the work if the requisition fail", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		srv := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+		response := httptest.NewRecorder()
+
+		srv.ServeHTTP(response, request)
+
+		store.assertWasCancelled()
+	})
+}
