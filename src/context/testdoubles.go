c98132ace6ee5ac64a71fba045d9e20dd6b49e7b
Thiago Lopes <thiago.lopes.dev@gmail.com>
2021-03-26 00:25:10 -0300

    feat: add Fetch implementation fo SpyStore

--- src/context/testdoubles.go
+++ src/context/testdoubles.go
@@ -1,38 +1,59 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"net/http"
 	"testing"
 	"time"
 )
 
 // SpyStore let you do a simulation with a store
 type SpyStore struct {
-	response  string
-	cancelled bool
-	t         *testing.T
+	response string
+	t        *testing.T
 }
 
 // Fetch will return a answer with a small delay
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				s.t.Log("spy store was cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
 
-// Cancel will store the call
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
-}
-
-func (s *SpyStore) assertWasCancelled() {
-	s.t.Helper()
-	if !s.cancelled {
-		s.t.Errorf("store dont was alerted about the cancel")
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
 	}
 }
 
-func (s *SpyStore) assertWasNotCancelled() {
-	s.t.Helper()
-	if s.cancelled {
-		s.t.Errorf(`store was alerted to cancel`)
-	}
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
 }
