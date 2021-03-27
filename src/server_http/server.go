2158262eb5a878b57503dedadb3b16ddf929ebee
Thiago Lopes <thiago.lopes.dev@gmail.com>
2021-03-27 06:19:18 -0300

    feat: add serverHttp and add a interface for the server implementation

--- src/server_http/server.go
+++ src/server_http/server.go
@@ -5,6 +5,23 @@ import (
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+// StoragePlayer is a storage to save the points of players in memory
+type StoragePlayer interface {
+	GetPlayerPoints(name string) int
+}
+
+// PlayerServer is an interface http for the data of players
+type PlayerServer struct {
+	storage StoragePlayer
+}
+
+func (p *PlayerServer) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	player := r.URL.Path[len("/players/"):]
+
+	points := p.storage.GetPlayerPoints(player)
+	if points == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, points)
 }
