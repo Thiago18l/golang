b69df9c742b36029a60217434d72eb5ed8ec8732
Thiago Lopes <thiago.lopes.dev@gmail.com>
2021-03-27 07:14:01 -0300

    test: add test case for register victories

--- src/server_http/server_test.go
+++ src/server_http/server_test.go
@@ -8,12 +8,46 @@ import (
 )
 
 type PlayerStorage struct {
-	points map[string]int
+	points       map[string]int
+	registersOwn []string
 }
 
 func (p *PlayerStorage) GetPlayerPoints(name string) int {
 	points := p.points[name]
 	return points
+}
+
+func (p *PlayerStorage) RegisterOwn(name string) {
+	p.registersOwn = append(p.registersOwn, name)
+}
+func TestStorageWins(t *testing.T) {
+	storage := PlayerStorage{
+		map[string]int{},
+		nil,
+	}
+	server := &PlayerServer{&storage}
+	t.Run("should return status 'accepted' to calls of method POST", func(t *testing.T) {
+		player := "Joana"
+		request := newRequisitionRegister(player)
+
+		response := httptest.NewRecorder()
+
+		server.ServerHTTP(response, request)
+
+		verifyStatusCode(t, response.Code, http.StatusAccepted)
+
+		if len(storage.registersOwn) != 1 {
+			t.Errorf("verified %d calls into RegisterOwn, waited %d", len(storage.registersOwn), 1)
+		}
+		if storage.registersOwn[0] != player {
+			t.Errorf("dont register the owner correct, received %s, but expect %s", storage.registersOwn[0], player)
+		}
+	})
+}
+
+func newRequisitionRegister(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
+	return req
 }
 
 func TestGetPlayers(t *testing.T) {
@@ -23,6 +57,7 @@ func TestGetPlayers(t *testing.T) {
 			"thiago": 20,
 			"pedro":  10,
 		},
+		nil,
 	}
 	server := &PlayerServer{&storage}
 	t.Run("return the result from thiago", func(t *testing.T) {
