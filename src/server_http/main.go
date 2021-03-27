a189f0c1bd1175caffa63ef705de38cc63629766
Thiago Lopes <thiago.lopes.dev@gmail.com>
2021-03-27 06:19:38 -0300

    feat: add struct to save data in memory and more

--- src/server_http/main.go
+++ src/server_http/main.go
@@ -5,8 +5,17 @@ import (
 	"net/http"
 )
 
+// PlayerStorageInMemory store in memory the data about the players
+type PlayerStorageInMemory struct{}
+
+// GetPlayerPoints get the points from a player
+func (p *PlayerStorageInMemory) GetPlayerPoints(name string) int {
+	return 123
+}
+
 func main() {
-	treater := http.HandlerFunc(PlayerServer)
+	server := &PlayerServer{&PlayerStorageInMemory{}}
+	treater := http.HandlerFunc(server.ServerHTTP)
 	if err := http.ListenAndServe(":8080", treater); err != nil {
 		log.Fatalf("Dont was possible to run on port 8080, %v", err)
 	}
