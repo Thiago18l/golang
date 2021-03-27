0463777767436fc255a471d797d3228cd997802e
Thiago Lopes <thiago.lopes.dev@gmail.com>
2021-03-27 06:18:22 -0300

    test: add helpers and some structs to get methods

--- src/server_http/server_test.go
+++ src/server_http/server_test.go
@@ -1,23 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+type PlayerStorage struct {
+	points map[string]int
+}
+
+func (p *PlayerStorage) GetPlayerPoints(name string) int {
+	points := p.points[name]
+	return points
+}
+
 func TestGetPlayers(t *testing.T) {
+
+	storage := PlayerStorage{
+		map[string]int{
+			"thiago": 20,
+			"pedro":  10,
+		},
+	}
+	server := &PlayerServer{&storage}
 	t.Run("return the result from thiago", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/thiago", nil)
+
+		request := newRequisitionPoints("thiago")
 		response := httptest.NewRecorder()
 
-		PlayerServer(response, request)
+		server.ServerHTTP(response, request)
 
 		received := response.Body.String()
 		waited := "20"
 
-		if received != waited {
-			t.Errorf("received '%s', but expect '%s'", received, waited)
-		}
+		verifyBodyRequest(t, received, waited)
 	})
+
+	t.Run("return the result from pedro", func(t *testing.T) {
+		request := newRequisitionPoints("pedro")
+		response := httptest.NewRecorder()
+
+		server.ServerHTTP(response, request)
+
+		received := response.Body.String()
+		waited := "10"
+
+		verifyBodyRequest(t, received, waited)
+	})
+}
+
+func newRequisitionPoints(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return request
+}
+
+func verifyBodyRequest(t *testing.T, received, waited string) {
+	t.Helper()
+	if received != waited {
+		t.Errorf(`received '%s', but expect '%s'`, received, waited)
+	}
 }
