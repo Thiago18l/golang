3c7d11a80b75bb52bd2eac012b4dc9c11af60606
Thiago Lopes <thiago.lopes.dev@gmail.com>
2021-03-27 07:15:03 -0300

    feat: add registerOwn method and add on interface

--- src/server_http/server.go
+++ src/server_http/server.go
@@ -8,6 +8,7 @@ import (
 // StoragePlayer is a storage to save the points of players in memory
 type StoragePlayer interface {
 	GetPlayerPoints(name string) int
+	RegisterOwn(name string)
 }
 
 // PlayerServer is an interface http for the data of players
@@ -17,11 +18,27 @@ type PlayerServer struct {
 
 func (p *PlayerServer) ServerHTTP(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
+	switch r.Method {
+	case http.MethodPost:
+		p.registerOwn(w, player)
+	case http.MethodGet:
+		p.showPoints(w, player)
+	}
+}
+
+func (p *PlayerServer) showPoints(w http.ResponseWriter, player string) {
 
 	points := p.storage.GetPlayerPoints(player)
+
 	if points == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
 	fmt.Fprint(w, points)
+}
+
+func (p *PlayerServer) registerOwn(w http.ResponseWriter, name string) {
+
+	p.storage.RegisterOwn(name)
+	w.WriteHeader(http.StatusAccepted)
 }
