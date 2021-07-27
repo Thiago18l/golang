7c9ad7d59708db6ca6c970b75d04ec69bb7b1771
Thiago L <thiago.lopes.dev@gmail.com>
2021-07-27 19:04:42 -0300

    feat: increment with pointer

--- src/go_book/pointers/pointer.go
+++ src/go_book/pointers/pointer.go
@@ -14,9 +14,18 @@ func main() {
 	var j, k int
 	fmt.Println(&j == &j, &j == &k, &x == nil)
 	fmt.Println(f() == f())
+
+	i := 1
+	increment(&i)              // side effect: i is now 2
+	fmt.Println(increment(&i)) // 3 and i is 3
 }
 
 func f() *int {
 	v := 1
 	return &v
+}
+
+func increment(p *int) int {
+	*p++ // increments what p points to; does not change p
+	return *p
 }
