cc028ecc49207486cc8e839120f58d7c2c017b3f
Thiago L <thiago.lopes.dev@gmail.com>
2021-07-29 01:16:52 -0300

    refactor: update func's and put Celsius to Fahrenheit

--- src/go_book/ftoc/transform.go
+++ src/go_book/ftoc/transform.go
@@ -3,12 +3,34 @@ package main
 
 import "fmt"
 
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsolutZeroC Celsius = -273.15
+	FreezingC    Celsius = 0
+	BoilingC     Celsius = 100
+)
+
 func main() {
 	const freezingF, boilingF = 32.0, 212.0
 	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF))
 	fmt.Printf("%g°F = %g°C\n", boilingF, fToC(boilingF))
+
+	// Celsius to Fahrenheit
+	fahrenheit := CtoF(-40)
+	fmt.Printf("%g°F\n", fahrenheit)
+
+	// fahrenheit to celsius
+	celsius := fToC(32)
+	fmt.Printf("%g°C\n", celsius)
+
 }
 
-func fToC(f float64) float64 {
-	return (f - 32) * 5 / 9
+func fToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
+
+func CtoF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
 }
